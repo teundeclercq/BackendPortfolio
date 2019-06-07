2187629f14f49c526a6a8f965911af6c49150263
Teun de Clercq <t.declercq@student.fontys.nl>
2019-06-07 15:06:40 +0200

    Added config file for Cors

---
+++ src/main/java/nl/teundeclercq/portofolio/config/CorsFilter.java
@@ -0,0 +1,16 @@
+package nl.teundeclercq.portofolio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class CorsFilter {
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("https://portfolios4.teun-school.nl");
+            }
+        };
+    }
+}
--- src/main/java/nl/teundeclercq/portofolio/controller/PortfolioController.java
+++ src/main/java/nl/teundeclercq/portofolio/controller/PortfolioController.java
@@ -17,9 +17,6 @@ import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/Portfolio")
-@CrossOrigin(maxAge = 3600,
-             methods = {RequestMethod.GET,  RequestMethod.DELETE, RequestMethod.POST, RequestMethod.PUT},
-             allowedHeaders = {"Access-Control-Allow-Origin"})
 public class PortfolioController {
     private static final Logger logger = Logger.getLogger(PortfolioController.class.getName());
     private static String exceptionMsg = "Exception";
